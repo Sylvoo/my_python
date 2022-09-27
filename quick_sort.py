af501b52a474c7e569c5793ab60391be0a396d28
Sylvoo <87396070+Sylvoo@users.noreply.github.com>
2022-09-27 20:22:58 +0200

    update

--- quick_sort.py
+++ quick_sort.py
@@ -2,19 +2,14 @@ tab = [5, 6, 30, 15, 44, 33, 98]
 
 def quicksort(tab):
     pivot = tab[len(tab) // 2]
-    sorted = []
+    sort = []
     for i in range(0,7):
         if tab[i] < pivot:
-            sorted.append(tab[i])
-            i+=1
-        elif tab[i] == pivot:
-            sorted.append(tab[i])
-            i+=1
-        elif tab[i] > pivot:
-            sorted.append(pivot)
-            pivot = tab[i]
-            i+=1
-        print(sorted)
-    print(sorted)
+            sort.append(tab[i])
+        elif tab[i] >= pivot:
+            sort.append(pivot)
+            pivot = tab[i
+        print(sort)
+    print(sort)
 
 quicksort(tab)
